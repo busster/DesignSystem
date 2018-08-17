d0b9ec93e19fc3ea2024e6f5c205186eafa4fd61
busster <jasonmbuss@gmail.com>
2018-08-16 21:33:20 -0500

    more naming

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-import Button from './Button'
+import BButton from './Button/BButton.vue'
 
 export {
-  Button
+  BButton
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+// import * as Components from './components'
+
+// let DesignSystem = Vue => {
+//   Object.values(Components).forEach((Component) => {
+//     Vue.use(Component)
+//   })
+// }
+
+// export default DesignSystem
+
 import Vue from 'vue'
 import * as Components from './components'
 
-Object.keys(Components).forEach(componentName => {
-  Vue.component(componentName, Components[componentName])
+Object.keys(Components).forEach(name => {
+  Vue.component(name, Components[name])
 })
 
 export default Components
