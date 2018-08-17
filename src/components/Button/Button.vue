de51ffb583dc967e915e696f8483ded3b51b1e25
busster <jasonmbuss@gmail.com>
2018-08-16 20:27:45 -0500

    styling

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -15,4 +15,19 @@ export default {
 </script>
 
 <style>
+button {
+  border: 1px solid;
+  border-radius: 3px;
+  padding: 0.5em 1em;
+  cursor: pointer;
+}
+button:hover {
+  background-color: aquamarine;
+}
+button:focus {
+  outline: none;
+}
+button:active {
+  background-color: aqua;
+}
 </style>
